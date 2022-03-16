80b6952be47e70f935f90df74275d53867f6b9ad
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2022-03-16 20:31:49 -0300

    Inclusão do projeto com formulário de componentes controlados

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type state = {
+  pnome: string,
+  unome: string;
+  password: string;
+  email: string
 }
+class App extends Component<any, state> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      pnome: '',
+      unome: '',
+      password: '',
+      email: ''
+    }
+  }
 
+  eventoFormulario = (evento: any) => {
+    evento.preventDefault()
+  }
+
+  obterPnome = (evento: any) => {
+    this.setState({
+      pnome: evento.target.value
+    })
+    console.log(this.state.pnome)
+  }
+
+  obterUnome = (evento: any) => {
+    this.setState({
+      unome: evento.target.value
+    })
+    console.log(this.state.unome)
+  }
+
+  obterPassword = (evento: any) => {
+    this.setState({
+      password: evento.target.value
+    })
+    console.log(this.state.password)
+  }
+
+  obterEmail = (evento: any) => {
+    this.setState({
+      email: evento.target.value
+    })
+    console.log(this.state.email)
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <form className="col s12" onSubmit={this.eventoFormulario}>
+          <div className="row">
+            <div className="input-field col s6">
+              <input value={this.state.pnome} onChange={this.obterPnome} id="first_name" type="text" className="validate" />
+              <label htmlFor="first_name">Primeiro nome</label>
+            </div>
+            <div className="input-field col s6">
+              <input value={this.state.unome} onChange={this.obterUnome} id="last_name" type="text" className="validate" />
+              <label htmlFor="last_name">Último nome</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input disabled value="Não sou editável" id="disabled" type="text" className="validate" />
+              <label htmlFor="disabled">Campo desabilitado</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input value={this.state.password} onChange={this.obterPassword} id="password" type="password" className="validate" />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input value={this.state.email} onChange={this.obterEmail} id="email" type="email" className="validate" />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s12">
+              <button type="submit">Enviar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
 export default App;
